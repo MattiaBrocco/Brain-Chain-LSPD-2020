503a5a118afb67ed8786deca9847625d198673c8
Alessandra <alessandra.cancellari@student.h-farm.com>
2020-12-01 08:52:03 +0100

    'Eliminated'

--- main.py
+++
@@ -1,9 +0,0 @@
-from base import get_lyric
-
-artist = "Beatles"
-title= "Across the Universe"
-
-song = get_lyric(artist, title)
-
-print("{} by {}:".format(title, artist))
-print("{}".format(song))
