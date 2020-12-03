18415fa8f4430db40ac292bd05e850c1719121b9
Eleonora Sartori <you@example.com>
2020-12-03 19:29:06 +0100

    Fixed the style to make it PEP-8 compliant (checked with pycodestyle)

--- main.py
+++ main.py
@@ -7,71 +7,77 @@ from lyrics_package import Songsterr
 
 
 def parsing_input():
-    #Parsing the input for our program.
-    #This function requires two positional arguments
-    
+    # Parsing the input for our program.
+    # This function requires two positional arguments
+
     parser = argparse.ArgumentParser()
-    #positional arguments
-    parser.add_argument("artist", help = 'the arstist of song you are searching for')
-    parser.add_argument("title", help = 'the title of the song you are searching for')
-    
-    #optional arguments
+    # positional arguments
+    parser.add_argument("artist",
+                        help='the arstist of song you are searching for')
+    parser.add_argument("title",
+                        help='the title of the song you are searching for')
+
+    # optional arguments
     parser.add_argument("-v",
                         help='increases verbosity of the program',
-                        action="store_true") # if the user doesn't input "-v" then verbosity is False
-    parser.add_argument("-history", help = "Show most searched songs",  ############
-                        action = "store_true") # if the user doesn't input "-v" then verbosity is False
+                        action="store_true")
+    # if the user doesn't input "-v" then verbosity is False
+    parser.add_argument("-history", help="Show most searched songs",
+                        action="store_true")
+    # if the user doesn't input "-v" then verbosity is False
     args = parser.parse_args()
     return args
 
+
 try:
     args = parsing_input()
-    
+
     # --------------------------------
     # Search history
     hist = History.create_hist(args.artist, args.title)
     if args.history:
         print("Most searched songs")
         print(hist)
-        
+
     # --------------------------------
     # LYRICS
     song_l = Lyrics.get_lyric(args.artist, args.title)
-    
-    if len(sys.argv)>4:
+
+    if len(sys.argv) > 4:
         print("**Use underscores (_) instead of spaces**")
         sys.exit()
 
-    if args.v: # verbosity = False
+    if args.v:  # verbosity = False
         print("This is {} by {}:".format(args.title, args.artist))
         print("{}\n\n".format(song_l))
     else:
-        print("{}\n\n".format(song_l))   
-       
+        print("{}\n\n".format(song_l))
+
     # --------------------------------
     # SONGSTERR
     song_t = Songsterr.artists_songs(args.artist, args.title)
 
-    if args.v: # verbosity = False
+    if args.v:  # verbosity = False
         print("Link to tabs:{}\n\n".format(song_t[0]))
-        print("Some songs from *{}* you might be interested in:".format(args.artist.capitalize()))
-        print(song_t[1],"\n\n")
+        print("Some songs from *{}* you might be interested in:"
+              .format(args.artist.capitalize()))
+        print(song_t[1], "\n\n")
     else:
         print("Link to tabs:{}\n\n".format(song_t[0]))
-        
+
     # --------------------------------
     # SEARCHLY
     sim_song = SearchLy.similarity(args.artist, args.title)
     print("{}".format(sim_song))
-    
+
 except ValueError:
     # ValueError includes the JSONDecodeError,
     # which sometimes arises from API calls
     print("Something wrong, please retry")
     sys.exit()
-    
+
 except PermissionError:
     print("Close the history.csv file before running the program!")
-    
+
 except ConnectionError:
     print("Bad conncetion error!")
