061282a8262b471b014a53aeeb26aaab67f9d6fe
Eleonora Sartori <you@example.com>
2020-12-03 22:23:00 +0100

    Fixed style to be PEP-8 compliant (checked with pycodestyle)

--- lyrics_package/History.py
+++ lyrics_package/History.py
@@ -12,50 +12,61 @@ def create_hist(artist, title):
     Check if the file already exists: in case it does
     append an additional line to the file, otherwise
     write ex-novo a file, adds the fiels (column names),
-    and the new line. The check is performed in the directory of this file, that
-    will be the same directory in which the .csv will be stored.
+    and the new line. The check is performed in the directory of this file,
+    that will be the same directory in which the .csv will be stored.
     """
-    csv_dir = os.path.dirname(os.path.realpath(__file__))   
+    csv_dir = os.path.dirname(os.path.realpath(__file__))
     if path.exists(csv_dir + "\\" + "history.csv") is False:
-        with open(csv_dir + "\\" + "history.csv", "w", newline = "") as csvfile:
+        with open(csv_dir + "\\" + "history.csv", "w", newline="")
+        as csvfile:
             # Add column names with this list
             fields = ["Artist", "Title", "Datetime"]
             # Based on DictWriter, a new line with header is added.
             # Then, the actual row is appended to the file
-            writer = csv.DictWriter(csvfile, fieldnames = fields)
+            writer = csv.DictWriter(csvfile, fieldnames=fields)
             writer.writeheader()
-            writer.writerow({"Artist": artist, "Title": title, 
-                             "Datetime": datetime.now().strftime("%d/%m/%y %H:%M:%S")})
+            writer.writerow({"Artist": artist, "Title": title,
+                             "Datetime": datetime.now()
+                             .strftime("%d/%m/%y %H:%M:%S")})
     else:
-        with open(csv_dir + "\\" + "history.csv", "a", newline = "") as csvfile:  # "a" stands for append
+        with open(csv_dir + "\\" + "history.csv", "a", newline="")
+        as csvfile:  # "a" stands for append
             writer = csv.writer(csvfile)
             # Once the file is already there, it is only necessary to
             # append the row through a list
-            new_row = [artist, title, datetime.now().strftime("%d/%m/%y %H:%M:%S")]
-            writer.writerow(new_row)     
+            new_row = [artist, title, datetime.now()
+                       .strftime("%d/%m/%y %H:%M:%S")]
+            writer.writerow(new_row)
     df = pd.read_csv(csv_dir + "\\" + "history.csv")
     """
     We created a list of tuples from the dataframe to allow
     easier comparison, beacuse they needed to be sorted (with Counter).
-    A nested list is produced and then reversed so that 
+    A nested list is produced and then reversed so that
     the most searched parameters will be displayed first.
     """
-    art_tit_list = [(r[1] [0], r[1] [1]) for r in df.iterrows()]
+    art_tit_list = [(r[1][0], r[1][1]) for r in df.iterrows()]
     sorted_dict = dict(Counter(art_tit_list))
-    list_for_df = [[k[0].replace("_", " "), k[1].replace("_", " ")] for k, v in sorted_dict.items()] [::-1]
-    upd_df = pd.DataFrame(list_for_df, columns = ["----------------","----------------"]).head(3)
+    list_for_df = [[k[0].replace("_", " "), k[1].replace("_", " ")]
+                   for k, v in sorted_dict.items()][::-1]
+    upd_df = pd.DataFrame(list_for_df,
+                          columns=["----------------", "----------------"])
+    .head(3)
     # CODE FOR MOST SEARCHED QUERIES
-    art_tit_list = [ (r[1][0], r[1][1]) for r in df.iterrows() ]
+    art_tit_list = [(r[1][0], r[1][1]) for r in df.iterrows()]
     sorted_dict = dict(Counter(art_tit_list))
-     # show only artist and song
-    list_for_df = [ [k[0].replace("_", " "), k[1].replace("_", " ")] for k, v in sorted_dict.items() ]
-    upd_df = pd.DataFrame(list_for_df, columns = ["----------------","----------------"]).head(3)    
-    
+    # show only artist and song
+    list_for_df = [[k[0].replace("_", " "),
+                    k[1].replace("_", " ")] for k, v in sorted_dict.items()]
+    upd_df = pd.DataFrame(list_for_df,
+                          columns=["----------------", "----------------"])
+    .head(3)
+
     """
     # CODE FOR MOST RECENT QUERIES
     upd_df = df.iloc[::-1]
     upd_df = upd_df.drop_duplicates(subset = ["Artist", "Title"],
-                                    keep = "first") # first because it would be the most recent
+                                    keep = "first")
+                                    # first because it would be the most recent
     upd_df.reset_index(inplace = True, drop = True)
     upd_df = upd_df[["Artist", "Title"]]
     upd_df = upd_df.head(3)
@@ -64,5 +75,5 @@ def create_hist(artist, title):
     upd_df = upd_df.rename(columns = {"Artist":"----------------",
                                       "Title":"----------------"})
     """
-    
+
     return upd_df
