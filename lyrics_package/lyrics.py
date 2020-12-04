41f713f2f9611ad585143131662fc91d0fa6d15c
Anna Costa <anna.costa@student.h-farm.com>
2020-12-04 23:12:32 +0100

    added docstrings and pep8 compliant

--- lyrics_package/lyrics.py
+++ lyrics_package/lyrics.py
@@ -1,11 +1,17 @@
 import requests
 import json
 
+
 def get_lyric(artist, title):
-    
-    artist=artist.replace("_"," ")
-    title=title.replace("_"," ")
-    
+    """
+    With this function we will obtain the lyrics of the song the user
+    has searched for. The user input that will start the function
+    are the artist name and the title of the song he are looking for.
+    """
+
+    artist = artist.replace("_", " ")
+    title = title.replace("_", " ")
+
     song_url = 'https://api.lyrics.ovh/v1/{}/{}'
 
     url = song_url.format(artist, title)
