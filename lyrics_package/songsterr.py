2858c3c1575e9acb39e2c1fbd0d8412b6db97233
Anna Costa <anna.costa@student.h-farm.com>
2020-12-04 22:55:30 +0100

    added docstrings and pep8 compliant

--- lyrics_package/songsterr.py
+++ lyrics_package/songsterr.py
@@ -10,8 +10,7 @@ def artists_songs(artist, title):
     the artist of the song, these inputs will be the variable
     that will be inserted in the API to get the output.
     In this case, the output will be the URL of the song sheet
-    and a data frame containing three similar songs to the one
-    that has been searched.
+    and a dataframe containing other five songs of the same artist.
     """
 
     artist = artist.replace("_", " ")
@@ -27,19 +26,29 @@ def artists_songs(artist, title):
     else:
         tabs_0 = "http://www.songsterr.com/a/wa/"
         tabs_1 = "bestMatchForQueryString?s={}&a={}"
-        
+
         # Whitespaces are replaced with "%20" as required
         # by the API in order to make the link exploitable
         url_format = tabs_0 + tabs_1.format(title.replace(" ", "%20"),
                                             artist.replace(" ", "%20"))
-        
+
+        """
+        In Songsterr, "datass" contains all the songs of the artist
+        the user is searching for. We will search in datass the id of
+        the artist in order to find the songs.
+        "nl" is a list of tuples which has the name of the artist as
+        the first element, and it has the types of song sheets as second
+        element. This let the user obtain as output only the other five
+        songs of the artist which have the songs sheets in Songsterr.
+        The list of songs will be displayed in a dataframe.
+        """
         nl = []
         for x in datass:
             if x["chordsPresent"] is True:
-                nl += [ (x["title"], len(x["tabTypes"])) ]
-        
+                nl += [(x["title"], len(x["tabTypes"]))]
+
         nl.sort(key=lambda x: x[1], reverse=True)
         df = pd.DataFrame({"-----------------\
         ---------------": [x[0] for x in nl[:5]]})
-        
+
         return (url_format, df)
